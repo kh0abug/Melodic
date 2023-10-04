3f4442434f37fa969b4c506fa3c99b6488bd9a20
Tran Dang Khoa <134161378+kh0abug@users.noreply.github.com>
2023-10-04 12:38:49 +0700

    Delete Melodic.Infracstucture directory
    Sai tên

--- Melodic.Infracstucture/Identity/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Melodic.Domain.ValueObjects;
-using Microsoft.AspNetCore.Identity;
-
-namespace Melodic.Infracstructure.Identity;
-public class ApplicationUser : IdentityUser
-{
-    public IReadOnlyCollection<Payment>? Payments { get; set; }
-}
