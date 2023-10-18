b8141cbc1b1aa52d1867565123c276bf3e9fd6cf
William <87595769+kiele4456@users.noreply.github.com>
2023-10-18 11:09:36 +0700

    Add Quantity Field

---
+++ Melodic.Infrastructure/Persistence/Migrations/20231018040845_AddQuantityField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Melodic.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuantityField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Speakers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Speakers");
+        }
+    }
+}
