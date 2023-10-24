560e68628f410e2e9474402d04a9f7cd4019077a
khoabug <kd031212@gmail.com>
2023-10-24 23:51:09 +0700

    fix can't add role when login with facebook

--- Melodic.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Melodic.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -163,6 +163,7 @@ namespace Melodic.Web.Areas.Identity.Pages.Account
                     result = await _userManager.AddLoginAsync(user, info);
                     if (result.Succeeded)
                     {
+                        await _userManager.AddToRoleAsync(user, ApplicationRole.Role_Customer);
                         _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
 
                         var userId = await _userManager.GetUserIdAsync(user);
