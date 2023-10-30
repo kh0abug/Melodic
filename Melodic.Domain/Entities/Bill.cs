64f2442c2204e44981e369886f2c2c31350acc59
wnosphan <137595499+wnosphan@users.noreply.github.com>
2023-10-30 10:44:17 +0700

    add bill

---
+++ Melodic.Domain/Entities/Bill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melodic.Domain.Entities
+{
+     public class Bill
+    {
+        public string? BuildId { get; set; }
+        public string? IdUser { get; set; }
+        public string? FullName { get; set; }
+        public string? Address { get; set; }
+        public double? Tax { get; set; }
+        public double? Discount { get; set; }
+        public double? Total { get; set; }
+        public double? TotalPrice { get; set; }
+        public string? Payment {  get; set; }
+        
+    }
+}
